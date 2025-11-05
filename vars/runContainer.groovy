4853607f246f77a9e640203476397512aedfb54b
venkat-6666 <venkatg01234@gmail.com>
2025-11-05 18:59:23 +0530

    relove

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,9 +1,9 @@
 def call(String imageName, String containerName, int hostPort, int containerPort) {
     stage('Docker Run') {
-        echo "🚀 Running container '${containerName}' from image '${imageName}' ..."
+        echo "Running container '${containerName}' from image '${imageName}' ..."
         sh """
            # Run new container
-            echo "🐳 Starting new container..."
+            echo "Starting new container..."
             docker run -d --name ${containerName} -p ${hostPort}:${containerPort} ${imageName}
         """
         echo "✅ Container '${containerName}' is running on port ${hostPort}"
